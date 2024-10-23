5a48f72d0bb8429d061d4104620bdc8ac9b52f34
Msyaiful09 <msyaiful703@gmail.com>
2024-10-23 17:09:33 +0700

    update sibaper

--- database/migrations/2024_09_25_082151_create_sibaper_table.php
+++ database/migrations/2024_09_25_082151_create_sibaper_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('berita_acara', function (Blueprint $table) {
             // $table->integer('nip')->primary();
-            $table->integer('nip');
             $table->bigInteger('nip')->primary();
             $table->integer('id_jadwal');
             $table->integer('id_rps');
