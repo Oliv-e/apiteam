436bb635dcdbe4ea804d99058a972cca3279a6d3
Oliv-e <oliversmk7rpl@gmail.com>
2024-10-24 15:52:38 +0700

    nambah model marked

--- app/Models/markeddokumen.php
+++ app/Models/markeddokumen.php
@@ -2,19 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class markeddokumen extends Model
+class MarkedDokumen extends Model
 {
-    use HasFactory;
-    protected $table = 'user';
+    protected $table = 'marked_dokumen';
     protected $fillable = [
-        'id_tandai',
-        'id_user',
-        'id_kategori',
-        'marked_at'
+        'id_dosen',
+        'id_dokumen'
     ];
+
+    public function dosen() {
+        return $this->belongsTo(Dosen::class, 'id_dosen', 'nip');
+    }
+    public function dokumen() {
+        return $this->belongsTo(Dokumen::class, 'id_dokumen', 'id');
+    }
 }
-
-
