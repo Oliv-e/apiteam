53e47663aeb4f34d4a4e08603aff14c60a8fe7a2
Oliv-e <oliversmk7rpl@gmail.com>
2024-10-28 12:35:22 +0700

    comment unused route

--- routes/api/sibaper.php
+++ routes/api/sibaper.php
@@ -16,31 +16,20 @@ Route::get('/', function() {
 Route::get('/data/homepage', [SibaperController::class, 'Homepage']);
 Route::get('/data/historypage', [SibaperController::class, 'Historypage']);
 Route::post('/data/BeritaAcara/tambah', [SibaperController::class, 'berita_acara_create']);
-
-
-Route::get('/data/kelas', [SibaperController::class , 'kelas']);
-Route::post('/data/Kelas/tambah', [KelasController::class , 'kelas']);
-
-Route::get('/data/Dosen', [DosenController::class , 'dosen']);
-Route::post('/data/Dosen/tambah', [DosenController::class , 'dosen']);
-
-Route::get('/data/Jadwal', [JadwalController::class , 'jadwal']);
-Route::post('/data/Jadwal/tambah', [JadwalController::class , 'jadwal']);
-
-Route::get('/data/Matkul', [MatkulController::class , 'matkul']);
-Route::post('/data/matkul/tambah', [MatkulController::class , 'matkul']);
-
-Route::get('/data/ruang', [RuangController::class , 'ruang']);
-Route::post('/data/ruang/tambah', [RuangController::class , 'ruang']);
-
 Route::get('/kelas-by-dosen-by-today', [SibaperController::class, 'kelas_by_dosen_today']);
+Route::get('/data/Jadwal', [JadwalController::class , 'jadwal']);
+Route::get('/data/rps', [RpsController::class, 'index']); // Untuk mengambil semua data RPS
+Route::get('rps/{id}', [RpsController::class, 'show']); // Untuk mengambil data RPS berdasarkan ID
 
-// // Route::get('/data/BeritaAcara/show/{id}', [BeritaAcaraController::class, 'berita_acara_show']);
+// Route::post('/data/Jadwal/tambah', [JadwalController::class , 'jadwal']);
+// Route::get('/data/Matkul', [MatkulController::class , 'matkul']);
+// Route::post('/data/matkul/tambah', [MatkulController::class , 'matkul']);
+// Route::get('/data/ruang', [RuangController::class , 'ruang']);
+// Route::post('/data/ruang/tambah', [RuangController::class , 'ruang']);
+// Route::get('/data/BeritaAcara/show/{id}', [BeritaAcaraController::class, 'berita_acara_show']);
 // Route::get('/data/BeritaAcara', [BeritaAcaraController::class , 'berita_acara']);
 // Route::post('/data/BeritaAcara/tambah', [BeritaAcaraController::class , 'berita_acara_create']);
 // Route::post('/data/BeritaAcara/update/{nip}', [BeritaAcaraController::class, 'berita_acara_update']);
 // Route::delete('/data/BeritaAcara/hapus/{nip}', [BeritaAcaraController::class, 'berita_acara_hapus']);
 
 
-Route::get('/data/rps', [RpsController::class, 'index']); // Untuk mengambil semua data RPS
-Route::get('rps/{id}', [RpsController::class, 'show']); // Untuk mengambil data RPS berdasarkan ID
